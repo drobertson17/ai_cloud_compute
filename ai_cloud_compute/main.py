d857d71c6984288c701f4bfc6f2d675fe93e3396
Dennis Robertson <dennis.m.robertson@gmail.com>
2024-11-25 18:22:38 -0500

    added test endpoint for image

--- ai_cloud_compute/main.py
+++ ai_cloud_compute/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+import base64
+from fastapi import FastAPI, File, UploadFile
+from uuid import uuid4
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# def image_to_base64(self, img_path: str) -> str:
+#     """Converts a local image file to base64 string."""
+#     with Image.open(img_path) as img:
+#         buffered = BytesIO()
+#         img.save(buffered, format="PNG")
+#         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+#     return img_str
+
+@app.post("/image-description")
+async def image_description(file: UploadFile = File()):
+    file.filename = f"{uuid4()}.jpg"
+    contents = await file.read()  # <-- Important!
+    img_str = base64.b64encode(contents).decode("utf-8")
+    return img_str
